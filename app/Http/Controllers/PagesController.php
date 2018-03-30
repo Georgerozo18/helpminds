ad24e455671d51254df333e000fa4ae0a6ec42ab
George <seergee18@gmail.com>
2018-03-30 10:50:58 -0500

    Registro de usuarios y mensajes de db

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Message;
+
+class PagesController extends Controller
+{
+    public function inicio(){
+      $messages = Message::latest()->paginate(10);
+      return view('welcome',
+        ['messages' => $messages,]);
+    }
+
+    public function acerca(){
+      return view('about');
+    }
+}
